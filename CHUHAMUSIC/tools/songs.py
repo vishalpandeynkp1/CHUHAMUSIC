4bdd2bfb3813f78f7f109cf241c6b658b6039adb
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 16:59:57 +0530

    Update songs.py

--- CHUHAMUSIC/tools/songs.py
+++ CHUHAMUSIC/tools/songs.py
@@ -1,12 +1,4 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 import os
 import re
