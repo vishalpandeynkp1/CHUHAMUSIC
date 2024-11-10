ba113d86afce8f3b4a36e837eed32dc8b85b771b
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-11-10 16:25:05 +0530

    Update resume.py

--- CHUHAMUSIC/plugins/admins/resume.py
+++ CHUHAMUSIC/plugins/admins/resume.py
@@ -1,20 +1,21 @@
 #
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+# Copyright (C) 2024 by vishalpandeynkp1@Github, < https://github.com/vishalpandeynkp1 >.
 #
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# This file is part of < https://github.com/vishalpandeynkp1/ > project,
 # and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+# Please see < https://github.com/vishalpandeynkp1/CHUHAMUSIC/blob/master/LICENSE >
 #
-# All rights reserved.
+# All rights reserved.A
+#
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
-from ERAVIBES import app
-from ERAVIBES.core.call import ERA as ERA
-from ERAVIBES.utils.database import is_music_playing, music_on
-from ERAVIBES.utils.decorators import AdminRightsCheck
+from CHUHAMUSIC import app
+from CHUHAMUSIC.core.call import CHUHA as CHUHA
+from CHUHAMUSIC.utils.database import is_music_playing, music_on
+from CHUHAMUSIC.utils.decorators import AdminRightsCheck
 
 
 @app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
@@ -23,7 +24,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await ERA.resume_stream(chat_id)
+    await CHUHA.resume_stream(chat_id)
     buttons_resume = [
         [
             InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
