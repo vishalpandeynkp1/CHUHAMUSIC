d525cf61d3550762e74d10793e8139261387193c
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-11-10 15:31:04 +0530

    Add files via upload

---
+++ CHUHAMUSIC/platforms/__init__.py
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .JioSavan import SaavnAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
