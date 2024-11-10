bb99e9aea92a54409e6a6767b606aaa5551bf40d
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 17:46:58 +0530

    Update help.py

--- CHUHAMUSIC/utils/inline/help.py
+++ CHUHAMUSIC/utils/inline/help.py
@@ -1,16 +1,9 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
+
 #
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_GROUP
-from ERAVIBES import app
+from CHUHAMUSIC import app
 
 
 def support_group_markup(_):
