7e6178e86a8bde9b827a567f46e7b3b33f9dba45
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 16:56:48 +0530

    Update ping.py

--- CHUHAMUSIC/tools/ping.py
+++ CHUHAMUSIC/tools/ping.py
@@ -1,23 +1,15 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS, PING_IMG_URL
-from ERAVIBES import app
-from ERAVIBES.core.call import ERA
-from ERAVIBES.utils import bot_sys_stats
-from ERAVIBES.utils.decorators.language import language
-from ERAVIBES.utils.inline import support_group_markup
+from CHUHAMUSIC import app
+from CHUHAMUSIC.core.call import CHUHA
+from CHUHAMUSIC.utils import bot_sys_stats
+from CHUHAMUSIC.utils.decorators.language import language
+from CHUHAMUSIC.utils.inline import support_group_markup
 
 
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
@@ -28,7 +20,7 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"].format(app.mention),
     )
     start = datetime.now()
-    pytgping = await ERA.ping()
+    pytgping = await CHUHA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
