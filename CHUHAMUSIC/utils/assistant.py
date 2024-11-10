4e10e068b90c47a6f8e864e40301ab86349151a0
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 17:52:40 +0530

    Update assistant.py

--- CHUHAMUSIC/utils/assistant.py
+++ CHUHAMUSIC/utils/assistant.py
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
-from ERAVIBES.utils.database import get_client
+
+from CHUHAMUSIC.utils.database import get_client
 
 
 async def get_assistant_details():
