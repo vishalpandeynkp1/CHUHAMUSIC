22c58bbd2b6ad9922511bc66dc1082da26f90854
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-11-10 15:54:42 +0530

    Add files via upload

---
+++ CHUHAMUSIC/plugins/misc/watcher.py
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from ERAVIBES import app
+from ERAVIBES.core.call import ERA
+
+
+@app.on_message(filters.video_chat_started, group=20)
+@app.on_message(filters.video_chat_ended, group=30)
+@app.on_message(filters.left_chat_member)
+async def force_stop_stream(_, message: Message):
+    try:
+        if message.left_chat_member and not message.left_chat_member is None:
+            if message.left_chat_member.id == (await get_assistant(message.chat.id)).id:
+                return await ERA.force_stop_stream(message.chat.id)
+        await ERA.force_stop_stream(message.chat.id)
+    except Exception:
+        pass
