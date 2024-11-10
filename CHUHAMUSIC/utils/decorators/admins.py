7373a5bc4bf1632b32c59d604ac4b093e86bf8fb
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 17:45:47 +0530

    Update admins.py

--- CHUHAMUSIC/utils/decorators/admins.py
+++ CHUHAMUSIC/utils/decorators/admins.py
@@ -1,21 +1,13 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 from pyrogram.enums import ChatMemberStatus, ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import adminlist
 from strings import get_string
-from ERAVIBES import app
-from ERAVIBES.misc import SUDOERS
-from ERAVIBES.utils.database import (
+from CHUHAMUSIC import app
+from CHUHAMUSIC.misc import SUDOERS
+from CHUHAMUSIC.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
