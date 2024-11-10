6aa6d5aa063ec9ee04852b8be6ff6923876dfe68
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 17:50:57 +0530

    Update stats.py

--- CHUHAMUSIC/utils/inline/stats.py
+++ CHUHAMUSIC/utils/inline/stats.py
@@ -1,15 +1,8 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
+
 #
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from ERAVIBES import app
+from CHUHAMUSIC import app
 
 
 def back_stats_markup(_):
