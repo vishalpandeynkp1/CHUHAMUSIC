91a0ca608c8e0b009c9a1587291d302153fa010b
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 17:55:58 +0530

    Update sys.py

--- CHUHAMUSIC/utils/sys.py
+++ CHUHAMUSIC/utils/sys.py
@@ -1,17 +1,10 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
+
 
 import time
 
 import psutil
 
-from ERAVIBES.misc import _boot_
+from CHUHAMUSIC.misc import _boot_
 
 from .formatters import get_readable_time
 
