7f9434e4beb9e7de384cd411b6896bdea5c3f619
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-11-10 16:32:20 +0530

    Update help.py

--- CHUHAMUSIC/plugins/bot/help.py
+++ CHUHAMUSIC/plugins/bot/help.py
@@ -1,5 +1,12 @@
-# All rights reserved.
 #
+# Copyright (C) 2024 by vishalpandeynkp1@Github, < https://github.com/vishalpandeynkp1 >.
+#
+# This file is part of < https://github.com/vishalpandeynkp1/CHUHAMUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/vishalpandeynkp1/CHUHAMUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import re
 from math import ceil
 from typing import Union
