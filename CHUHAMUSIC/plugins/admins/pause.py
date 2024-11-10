ef8431fe81da8c0e0eb172efb9f6ccf80befa5f7
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-11-10 16:24:10 +0530

    Update pause.py

--- CHUHAMUSIC/plugins/admins/pause.py
+++ CHUHAMUSIC/plugins/admins/pause.py
@@ -1,20 +1,21 @@
 #
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+# Copyright (C) 2024 by vishalpandeynkp1@Github, < https://github.com/vishalpandeynkp1 >.
 #
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# This file is part of < https://github.com/vishalpandeynkp1/CHUHAMUSIC > project,
 # and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+# Please see < https://github.com/vishalpandeynkp1//blob/master/LICENSE >
 #
-# All rights reserved.
+# All rights reserved.A
+#
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
-from ERAVIBES import app
-from ERAVIBES.core.call import ERA as ERA
-from ERAVIBES.utils.database import is_music_playing, music_off
-from ERAVIBES.utils.decorators import AdminRightsCheck
+from  import app
+from CHUHAMUSIC.core.call import CHUHA as CHUHA
+from CHUHAMUSIC.utils.database import is_music_playing, music_off
+from CHUHAMUSIC.utils.decorators import AdminRightsCheck
 
 
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
@@ -23,7 +24,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await ERA.pause_stream(chat_id)
+    await CHUHA.pause_stream(chat_id)
 
     buttons = [
         [
