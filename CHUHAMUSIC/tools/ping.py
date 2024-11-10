d89092f654a5879960a203236f9f70c93a171add
Dvis <gokhaledurgesh4@gmail.com>
2024-11-10 16:47:48 +0530

    Auto Fixes

---
+++ CHUHAMUSIC/tools/ping.py
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, PING_IMG_URL
+from ERAVIBES import app
+from ERAVIBES.core.call import ERA
+from ERAVIBES.utils import bot_sys_stats
+from ERAVIBES.utils.decorators.language import language
+from ERAVIBES.utils.inline import support_group_markup
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    start = datetime.now()
+    pytgping = await ERA.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=support_group_markup(_),
+    )
